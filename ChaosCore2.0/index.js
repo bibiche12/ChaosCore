290615a9845708c66911ef55564facbe8f68b857
bibiche12 <black.bibiche.co@gmail.com>
2026-06-10 09:57:31 +0200

    Update index.js

--- ChaosCore2.0/index.js
+++ ChaosCore2.0/index.js
@@ -104,14 +104,7 @@ client.once('clientReady', async () => {
 
     await db.initDatabase();
     await registerCommands();
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-await rest.put(
-    Routes.applicationCommands(process.env.CLIENT_ID),
-    { body: [] }
-);
-
-console.log('🧹 Commandes globales supprimées');
     await restoreDisboardReminder(client);
 
     const twitchChat = twitchService.createTwitchChat(client, sendContestLog);
